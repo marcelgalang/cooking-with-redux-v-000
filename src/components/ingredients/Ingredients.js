0dbc09f2fa7946e57412fa68bb60211177d8469d
github username <github email address>
2017-03-09 15:41:26 -0500

    Start solutions

--- src/components/ingredients/Ingredients.js
+++ src/components/ingredients/Ingredients.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+// import Ingredient from './Ingredient';
 
 export class Ingredients extends Component {
+
   render(){
+    const { store } = this.props;
+    const ingredients = store.getState().ingredients.map((ingredient, index) => {
+      return <Ingredient key={index} ingredient={ingredient} store={store} />
+    });
+
     return(
         <div>
           <ul>
-            List of Ingredients.
+            {ingredients}
           </ul>
         </div>
     )
--- src/components/ingredients/IngredientsInput.js
+++ src/components/ingredients/IngredientsInput.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 
 export class IngredientsInput extends Component {
-  render(){
-    return(
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  };
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_INGREDIENT',
+      ingredient: {
+        text: this.state.text,
+      },
+    });
+    this.setState({
+      text: ''
+    });
+  }
+
+  render() {
+    return (
       <div>
-        Ingredients Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)} >
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
-    )
+    );
   }
 }
 
